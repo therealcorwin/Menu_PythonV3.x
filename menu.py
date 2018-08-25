ec3b4bd0b911237db29532d88f6ee5bb00d7c254
Thubtenrigzin <thubtenrigzin@gmail.com>
2018-08-25 17:55:36 +0800

    bugs en moins...

--- menu.py
+++ menu.py
@@ -8,7 +8,7 @@ from os import system, name
 
 class Menu_item:
     def __init__(self, name, index, structure):
-        self.name = name
+        self.name = self.title = name
         self.index = index
         self.structure = structure
 
@@ -30,65 +30,86 @@ class Sub_menu_item(Menu_item):
 
 class Menu:
     def __init__(self, name, structure):
-        self.name = name
+        self.title = name
         self.structure = structure
+        self.walk_to_next_menu_list()
 
     def set_config(self, header, footer, border):
         self.bordure = border
         self.header = f" {header} "
         self.footer = f" {footer} "
 
+    def is_sub_menu(self):
+        return len(self.walk) > 1
+
     def set_structure(self):
         return [Menu_item(k, i, v) for i, el in enumerate(self.structure, 1) for k, v in el.items()]
 
-    def walker(self):
+    def walker(self, entry):
         if not hasattr(self, "walk"):
             self.walk = [self.set_structure()]
-        elif self.entry:
+        elif entry:
             if self.current_menu_elements[self.entry - 1].has_sub_menu():
                 self.walk.append(self.current_menu_elements[self.entry - 1].sub_menu)
         elif len(self.walk) > 1:  # Retour au précédant
             del self.walk[-1]
         self.current_menu_elements = self.walk[-1]
-        if len(self.walk) > 1:
-            self.name = self.current_menu_elements[self.entry - 1].name
+        self.name = self.current_menu_elements[self.entry - 1].parent.name if len(self.walk) > 1 else self.title
         return self.current_menu_elements
 
-    def get_menu_list(self):
-        return "\t\n".join(f"\t{e}" for e in self.walker())
+    def walk_to_next_menu_list(self, entry=None):
+        self.current_menu = "\t\n".join(f"\t{e}" for e in self.walker(entry))
 
     def __repr__(self):
         self.clear()
-        affichage = f"{self.header:{self.bordure}^60}\n{self.name}\n{self.get_menu_list()}\nVersion : {version}\n{self.footer:{self.bordure}^60}"
-        if self.walk:
+        menu = self.current_menu
+        if self.is_sub_menu():
+            menu += f"\n 0. Back"
+        affichage = f"{self.header:{self.bordure}^60}\n{self.name}\n{menu}\nVersion : {version}\n{self.footer:{self.bordure}^60}"
+        if self.is_sub_menu():
             affichage += f"\n 0. retour"
         return affichage
 
-    def __call__(self):
+    def keyboard_process(self):
         breaked = 0
         while True:
             try:
-                reply = input("Choice :")
-            except KeyboardInterrupt:
-                breaked = self.escape_menu()
-                continue
+                reply = input("Choice : ")
+            except (KeyboardInterrupt, ValueError) as e:
+                if "KeyboardInterrupt" in e.__repr__():
+                    print("keyboard")
+                    breaked = self.escape_menu()
+                elif "ValueError" in e.__repr__():
+                    print("Only numbers are allowed!")
             else:
+                if not reply: self()
                 try:
                     self.entry = int(reply)
                 except ValueError:
                     if reply.lower()[0] == "y" and breaked: exit(0)
+                    elif reply.lower()[0] == "n": self()
                     else:
                         print("Only numbers are allowed!")
-                else: break
+                else:
+                    if self.entry > len(self.current_menu_elements):
+                        print("exceed the value!")
+                    else:
+                        return self.entry
+
+    def __call__(self):
+        while True:
+            print(self)
+            self.walk_to_next_menu_list(self.keyboard_process())
+
+
+    @staticmethod
+    def clear():
+        _ = system('cls') if name == 'nt' else system('clear')
 
     def escape_menu(self):
         self.clear()
         print(f"{self.header:{self.bordure}^60}\nQuit?\nYes (y)\nVersion : {version}\n{self.footer:{self.bordure}^60}")
         return 1
-
-    @staticmethod
-    def clear():
-        _ = system('cls') if name == 'nt' else system('clear')
 
 
 if __name__ == '__main__':
@@ -99,6 +120,4 @@ if __name__ == '__main__':
 
     menu = Menu("Mon menu", config)
     menu.set_config("header", "footer", "=")
-    while True:
-        print(menu)
-        menu()
+    menu()
